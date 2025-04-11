756d15c195c18dc7bf2b57ade3c785c52f04d1d3
isabelpt <iaptny@gmail.com>
2025-04-11 13:32:28 -0400

    Basic

--- AboutMe/ContentView.swift
+++ AboutMe/ContentView.swift
@@ -9,11 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Text("Isabel")
+            .font(.largeTitle)
+        VStack(alignment: .leading, spacing: 20) {
+        HStack(alignment: .center, spacing: 20) {
+            Image(.isabel)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            Text("On land, I love to go hiking and touring farms in New Hampshire.").foregroundStyle(.green)
+        }
+        HStack(alignment: .center, spacing: 20) {
+            Image(.scuba)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            Text("In the ocean, I love going scuba diving and learning about new fish.").foregroundStyle(.blue)
+        }
+        Text("Wherever I am I love listening to music and spending time with friends and family!")
         }
         .padding()
     }
